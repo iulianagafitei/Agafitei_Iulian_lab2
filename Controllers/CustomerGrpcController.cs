9c5679400be07d194a3fb44cbb8fd9ef1a147ae9
iulian agafitei <agafitei.iulian.nicolae@gmail.com>
2021-01-07 19:11:26 +0200

    Lab11

---
+++ Controllers/CustomerGrpcController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Grpc.Net.Client;
+using GrpcCustomerService;
+
+namespace Agafitei_Iulian_lab2.Controllers
+{
+    public class CustomerGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public CustomerGrpcController()
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:5001");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new
+                CustomerService.CustomerServiceClient(channel);
+                var createdCustomer = client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+    }
+}
